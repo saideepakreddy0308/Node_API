bdbb028e315184476cba863bc0aa4669bc13e34c
Antino Labs <90776053+saideepakreddy0308@users.noreply.github.com>
2024-01-24 16:50:21 +0530

    post method product

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 
 const express = require("express");
 const app = express();
+const Product = require('./models/productModel');
+app.use(express.json());
 
 // routes
 app.get("/", (req, res) => {
@@ -12,8 +14,25 @@ app.get("/blog", (req, res) => {
   res.send("Hello Deepak!, Its my first blog page");
 });
 
+app.post('/product', (req, res) => {
+    // req is the request that's coming from the client, and res is the response that's being sent back from the server.
+  res.send('Hello Deepak!, Its my first product page');
+  console.log(req.body);
+  try {
+    const product = Product.create(req.body);
+    return res.status(201).json({
+      success: true,
+      data: product
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      success: false,
+      error: 'Server Error'
+    });
+  }
+});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
-
