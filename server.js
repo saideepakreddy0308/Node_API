0926b410ff2ae51f4bc199a66602f26c45ac75a3
Antino Labs <90776053+saideepakreddy0308@users.noreply.github.com>
2024-01-24 15:26:14 +0530

    nodemon setup

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.get("/", (req, res) => {
   res.send("Hello Node API, Deepak!");
 });
 
+app.get("/blog", (req, res) => {
+  res.send("Hello Deepak!, Its my first blog page");
+});
+
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
